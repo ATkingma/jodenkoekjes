c29774f627240366a8ed194258df37fc10293bd6
JKrediet <60423064+JKrediet@users.noreply.github.com>
2020-11-05 13:06:30 +0100

    character movement
    staat is jorn scene!
    test maar ff

---
+++ shooter/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // camera controls
+    private float rotX;
+    private float rotY;
+
+    // max rotation
+    public float maxLookUp;
+    public float maxLookDown;
+    public float rotSpeed;
+
+    // position
+    public GameObject player;
+
+
+    void Update()
+    {
+        transform.position = player.transform.position;
+        //rotation
+        rotX += Input.GetAxis("Mouse X") * rotSpeed;
+        rotY += Input.GetAxis("Mouse Y") * rotSpeed;
+
+        rotY = Mathf.Clamp(rotY, -maxLookDown, maxLookUp);
+        transform.rotation = Quaternion.Euler(-rotY, rotX, 0f);
+    }
+}
---
+++ shooter/Assets/Scripts/Movement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float movementSpeed, sprintSpeed, gravity, jumpForce, crouchHeight;
+
+    //privates
+    private CharacterController controller;
+    private Vector3 moveDir;
+
+    private float rotX, speed, groundedCooldown = 0.1f, downForce, bodyHeight;
+    private bool mayJump;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        bodyHeight = GetComponent<CharacterController>().height;
+    }
+
+    private void Update()
+    {
+        //jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (mayJump)
+            {
+                mayJump = false;
+                downForce = jumpForce;
+            }
+        }
+
+        //sprint
+        if (Input.GetButton("Sprint"))
+        {
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = movementSpeed;
+        }
+
+        //sprint
+        if (Input.GetButton("Crouch"))
+        {
+            GetComponent<CharacterController>().height = crouchHeight;
+        }
+        else
+        {
+            GetComponent<CharacterController>().height = bodyHeight;
+        }
+
+        //gravity
+        if (controller.isGrounded)
+        {
+            mayJump = true;
+            if (Time.time == groundedCooldown + Time.time)
+            {
+                downForce = -0.01f;
+                groundedCooldown = 0.1f;
+            }
+            else
+            {
+                groundedCooldown = 0;
+            }
+        }
+        else
+        {
+            downForce -= gravity * Time.deltaTime;
+        }
+
+        //movement
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), downForce, Input.GetAxisRaw("Vertical"));
+        moveDir = transform.TransformDirection(moveDir);
+        controller.Move(moveDir.normalized * movementSpeed * Time.deltaTime);
+
+        //rotation
+        rotX += Input.GetAxis("Mouse X") * FindObjectOfType<CameraController>().rotSpeed;
+        transform.rotation = Quaternion.Euler(0, rotX, 0f);
+    }
+}
