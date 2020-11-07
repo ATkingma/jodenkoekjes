e50caf0515ed8a2d25a3c141a0a17699daf8608d
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-11-07 22:37:05 +0100

    goblin enemie
    af?
    iemand moet ff na kijken zit een scene bij
    
    groene block is player
    
    enemie is de goblin
    
    geprobeert animaties mooi te maken maar kijk maar ff

---
+++ shooter/Assets/Timme/goblin enemie/EnemieScript.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemieScript : MonoBehaviour
+{
+    //public
+    public float attackCoolDown;
+    public Animator anim;
+    //private
+    private GameObject player;
+    private bool doingDamage, isAtacking, PlayerInTrigger;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void Update()
+    {
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist <= 4)
+        {
+
+            if (isAtacking == false)
+            {
+                if (PlayerInTrigger == true)
+                {
+                Attacking();
+                    UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+                    agent.destination = gameObject.transform.position;
+                }
+            }
+        }
+        if (dist >= 6.4f)
+        {
+            ResetAnim();
+            UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+            agent.destination = player.transform.position;
+        }
+    }
+    public void Attacking()
+    {
+        if (doingDamage == false)
+        {
+            DoDamage();
+            Atack();
+        }
+        print("Attacking");
+        isAtacking = true;
+    }
+    public void DoDamage()
+    {
+        Invoke("Resset", attackCoolDown);
+        Invoke("DoTaunt", attackCoolDown / 2);
+        print("DoDammage");
+        doingDamage = true;
+    }
+    public void DoTaunt()
+    {
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist <= 6.4f)
+        {
+            Taunt();
+        }
+    }
+    public void Resset()
+    {
+        ResetAnim();
+        isAtacking = false;
+        print("Resset");
+        doingDamage = false;
+    }
+    public void Taunt()
+    {
+        ResetAnim();
+        anim.SetBool("IsTaunting", true);
+    }
+    public void Atack()
+    {
+        ResetAnim();
+        anim.SetBool("IsAttacking", true);
+    }
+    public void ResetAnim()
+    {
+        anim.SetBool("IsAttacking", false);
+        anim.SetBool("IsTaunting", false);
+    }
+   public  void OnTriggerEnter(Collider gameobject)
+    {
+        if (gameobject.gameObject.tag == "Player")
+        {
+            PlayerInTrigger = true;
+        }
+    }
+    public void OntriggerExit(Collider gameobject)
+    {
+        if (gameobject.gameObject.tag == "Player")
+        {
+            PlayerInTrigger = false;
+        }
+    }
+}
