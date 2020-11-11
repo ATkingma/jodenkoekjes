6bcfee654e3ec287f641587c19da5615b4e0d219
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-11-11 15:34:09 +0100

    spawner

---
+++ shooter/Assets/Scripts/spawner/SpawnPoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public bool SpawnAble;
+    void Start()
+    {
+        SpawnAble = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider gameobject)
+    {
+        if (gameobject.gameObject.tag == "Wall")
+        {
+            SpawnAble = false;
+        }
+    }
+    public void OntriggerExit(Collider gameobject)
+    {
+        if (gameobject.gameObject.tag == "Wall")
+        {
+            SpawnAble = true;
+        }
+    }
+}
---
+++ shooter/Assets/Scripts/spawner/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //public
+    public List<GameObject> spawnPoints, enemie;
+    //private
+    private float SpawnCoolDown;
+    private bool isSpawning;
+    void Start()
+    {
+        spawnPoints.AddRange(GameObject.FindGameObjectsWithTag("SpawnPoint"));
+    }
+    void Update()
+    {
+        if (SpawnCoolDown <= Time.deltaTime)
+        {
+            if (isSpawning == false)
+            {
+            Spawn();
+            }
+        }
+    }
+    public void Spawn()
+    {
+        foreach (GameObject spawnPoint in spawnPoints)
+        {
+            if (spawnPoint.GetComponent<SpawnPoint>().SpawnAble == true)
+            {               
+               int enemiePrefab= Random.Range(0, enemie.Count);
+                      Instantiate(enemie[enemiePrefab], spawnPoint.transform.position, Quaternion.identity);
+            }
+        }
+        isSpawning = true;
+    }
+}
