88441e5130c9137dcb7445daca6c06347e1eabdb
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-11-22 14:06:50 +0100

    scene loader

---
+++ shooter/Assets/Scripts/Sceneringdingding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneringdingding : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void SceneLoader()
+    {
+        int sceneindex = Random.Range(1, 3);
+        SceneManager.LoadScene(sceneindex);
+    }
+}
