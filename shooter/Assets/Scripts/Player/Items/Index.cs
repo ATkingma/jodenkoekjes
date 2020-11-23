b1452d4d818e15d8429bb02c3cdd5f95c88cca90
JKrediet <60423064+JKrediet@users.noreply.github.com>
2020-11-23 22:56:56 +0100

    tab button fixed

--- shooter/Assets/Scripts/Player/Items/Index.cs
+++ shooter/Assets/Scripts/Player/Items/Index.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Index : MonoBehaviour
 {
     public List<GameObject> items, index, index2;
     public ItemList itemslist;
+
+    public Image itemindex, tab;
 
     public void AddItem()
     {
@@ -20,6 +23,18 @@ public class Index : MonoBehaviour
                     index2[i] = Instantiate(index[i], transform);
                 }
                 index2[i].GetComponentInChildren<TextMeshProUGUI>().text = itemslist.itemQuantity[i].ToString();
+            }
+        }
+    }
+    private void Update()
+    {
+        itemindex.enabled = Input.GetButton("TAB");
+        tab.enabled = Input.GetButton("TAB");
+        for (int i = 0; i < itemslist.itemQuantity.Count; i++)
+        {
+            if (itemslist.itemQuantity[i] > 0)
+            {
+                index2[i].SetActive(Input.GetButton("TAB"));
             }
         }
     }
