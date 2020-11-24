d0cedb45ff087dda1f0595ba29716b2233d9de50
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-11-24 11:21:20 +0100

    button

---
+++ shooter/Assets/Button.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    //public
+    public GameObject top,door;
+    //private
+    private bool triggert, isDown,dontDoAnny;
+    void Update()
+    {
+        if (isDown)
+        {
+            door.SetActive(false);
+        }
+        if (!dontDoAnny)
+        {
+
+        if (top.transform.position.y <= -0.332f)
+        {
+            isDown = true;
+                dontDoAnny = true;
+        }
+        if (triggert)
+        {
+            if (!isDown)
+            {
+                top.transform.position -= new Vector3(0, 0.3f, 0) * Time.deltaTime;
+            }
+        }
+    }
+      }
+    public void OnTriggerEnter(Collider gameobject)
+    {
+        if (gameobject.gameObject.tag == "Player")
+        {
+            triggert = true;
+        }
+    }
+}
