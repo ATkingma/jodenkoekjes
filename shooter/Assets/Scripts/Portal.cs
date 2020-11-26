1aa1cd4c0cb0ea43514e1f0a6c9aa133a733e50c
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-11-26 18:40:00 +0100

    fix dat er enemies nog van vorige keer zijn

--- shooter/Assets/Scripts/Portal.cs
+++ shooter/Assets/Scripts/Portal.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+    private GameObject[] enemies;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
             print("ringdigndijgnng");
             FindObjectOfType<SceneSwitcher>().SceneLoader();
+            GetEnemies();
+        }
+    }
+    public void GetEnemies()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        KillEnemie();
+    }
+    public void KillEnemie()
+    {
+        foreach (GameObject enemie in enemies)
+        {
+            Destroy(enemie);
         }
     }
 }
