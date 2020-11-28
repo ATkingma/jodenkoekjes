5424aadea47645e6feb221447b87d66d2ab8f265
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-11-28 01:26:33 +0100

    ik dacht de makker moet ook dood  tochh?

---
+++ shooter/Assets/DeathPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DeathPlayer : MonoBehaviour
+{
+    public GameObject player,deathUI;
+    void Update()
+    {
+        if (player.GetComponent<PlayerHealth>().health <= 0)
+        {
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            deathUI.SetActive(true);
+        }
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
