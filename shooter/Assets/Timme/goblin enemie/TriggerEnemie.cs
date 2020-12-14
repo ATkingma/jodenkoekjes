9d2d6d5c6081fa39244552b8f11304afb79b00de
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-12-14 15:40:18 +0100

    ikke ga vnv veder

--- shooter/Assets/Timme/goblin enemie/TriggerEnemie.cs
+++ shooter/Assets/Timme/goblin enemie/TriggerEnemie.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class TriggerEnemie : MonoBehaviour
 {
     //public
-    public bool goblin, groot, ranged,boss;
+    public bool goblin, groot, ranged,boss,golem;
     public GameObject enemy;
     public void OnTriggerEnter(Collider gameobject)
     {
@@ -13,7 +13,7 @@ public class TriggerEnemie : MonoBehaviour
         {
             if (goblin)
             {
-                enemy.GetComponent<EnemieScript>().PlayerInTrigger = true;
+                enemy.GetComponent<EnemieScript>().PlayerInTrigger = true; 
             }
             if (groot)
             {
@@ -26,6 +26,18 @@ public class TriggerEnemie : MonoBehaviour
             if (boss)
             {
                 enemy.GetComponent<Boss>().PlayerInTrigger = true;
+            }
+            if (golem)
+            {
+                enemy.GetComponent<Goblin>().PlayerInTrigger = true;
+            }
+        }
+        if (golem)
+        {
+            if (gameobject.gameObject.tag == "Goblin")
+            {
+                enemy.GetComponent<Goblin>().goblinInTrigger = true;
+                Destroy(gameobject);
             }
         }
     }
@@ -49,6 +61,32 @@ public class TriggerEnemie : MonoBehaviour
             {
                 enemy.GetComponent<Boss>().PlayerInTrigger = false;
             }
+            if (golem)
+            {
+                enemy.GetComponent<Goblin>().PlayerInTrigger = false;
+            }
+        }
+        if (golem)
+        {
+            if (gameobject.gameObject.tag == "Goblin")
+            {
+                enemy.GetComponent<Goblin>().goblinInTrigger = false;
+                //doe hier 1 en de bools
+            }
         }
     }
 }
+//als goblin in box zit
+//destroy die
+//en zet bool aan dat die aan het gooien is
+//als player in zit sla die
+//onder tussen gooi de goblins naar de player hoe
+
+//zet bool aan
+//pak hem op
+//zet de gene in de hand aan
+//kijk 1 keer naar de player voordat je gaat gooien
+//dan instantiate op het goeie moment op de positie in de hand die goblin met scripts enz
+//en geef zijn rigid body velocity omhoog zodat dat gebeurt met een soort boogje
+//zet bool uit
+
