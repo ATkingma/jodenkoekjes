4de2f28ddf1fe808b0084a1b799a4b124286dfd3
JKrediet <60423064+JKrediet@users.noreply.github.com>
2020-12-16 16:28:56 +0100

    veel succes timme!

--- shooter/Assets/Deactivate.cs
+++ shooter/Assets/Deactivate.cs
@@ -5,8 +5,12 @@ using UnityEngine.AI;
 
 public class Deactivate : MonoBehaviour
 {
+    private float speed;
     private NavMeshAgent agent;
     private CharacterController controller;
+    private GameObject player;
+    private Rigidbody rb;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -15,16 +19,23 @@ public class Deactivate : MonoBehaviour
         gameObject.GetComponent<Animator>().enabled = false;
         agent = GetComponent<NavMeshAgent>();
         agent.enabled = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+        transform.LookAt(player.transform);
+        rb = GetComponent<Rigidbody>();
+        speed = 100;
+        rb.velocity = transform.forward * speed;
     }
     private void Update()
     {
-        if (controller.isGrounded)
+        if (Physics.Raycast(transform.position, -Vector3.up, 1))
         {
             DoAchtive();
         }
     }
     public void DoAchtive()
     {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
         gameObject.GetComponent<EnemieScript>().enabled = true;
         gameObject.GetComponent<EnemyHealth>().enabled = true;
         gameObject.GetComponent<Animator>().enabled = true;
