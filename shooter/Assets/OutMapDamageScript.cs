1485aa63250b7aa723d760bf695566ee34fa3428
ATkingma <60423088+ATkingma@users.noreply.github.com>
2020-12-24 17:41:19 +0100

    outmapdamage script

---
+++ shooter/Assets/OutMapDamageScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutMapDamageScript : MonoBehaviour
+{
+    private void OnTriggerStay(Collider gameobject)
+    {
+        if (gameobject.gameObject.tag == "Player")
+        {
+            gameobject.GetComponent<PlayerHealth>().ReceiveDamage(2.2f*Time.deltaTime/0.15f, 0);
+        }
+    }
+}
