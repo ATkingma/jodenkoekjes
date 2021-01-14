4c27ba1a40276274ffe115d6d741f2c204389046
ATkingma <60423088+ATkingma@users.noreply.github.com>
2021-01-14 19:54:08 +0100

    winning zin in

---
+++ ever islens/Assets/MainMenuCamMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCamMovement : MonoBehaviour
+{
+    public GameObject[] checkPoints;
+    private int index=0;
+    // Update is called once per frame
+    public void OnTriggerEnter(Collider gameobject)
+    {
+        if (gameobject.gameObject.tag == "MovementBlock")
+        {
+            index++;
+        }
+    }
+    void Update()
+    {
+        if (index >= 24)
+        {
+            index = 0;
+        }
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = checkPoints[index].transform.position;
+    }
+}
---
+++ ever islens/Assets/MainMenuCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCamera : MonoBehaviour
+{
+    public GameObject lookAtObject;
+    void Update()
+    {
+        gameObject.transform.LookAt(lookAtObject.transform);
+    }
+}
