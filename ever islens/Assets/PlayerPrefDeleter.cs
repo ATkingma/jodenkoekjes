e3c07ba8724988449b37606f5e780227d82f6bf3
ATkingma <60423088+ATkingma@users.noreply.github.com>
2021-01-15 00:01:56 +0100

    boobala

---
+++ ever islens/Assets/PlayerPrefDeleter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefDeleter : MonoBehaviour
+{
+    void Update()
+    {
+    }
+    public void Delte()
+    {
+        Screen.fullScreen = true;
+        PlayerPrefs.DeleteAll();
+    }
+}
